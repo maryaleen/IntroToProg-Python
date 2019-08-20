466e2321d0e683fb9953658488184b020d1008ae
maryaleen <50726336+maryaleen@users.noreply.github.com>
2019-08-19 20:03:47 -0700

    Add files via upload
    Assignment 06. Refactor of Assignment 05 code.

--- ToDoList.py
+++ ToDoList.py
@@ -1,32 +1,79 @@
-# Assignment 05
+# Assignment 06
 # Title: To Do List
 # Description: Read data from text file, and allow user to add or
 # remove items, view or save To Do List data, and exit.
 # -------------------------- ChangeLog ---------------------------
 #   Developer:      Date:       Reason:
 #   MWilliams       08/14/2019  Assignment05 Initial Release
+#   MWilliams       08/18/2019  Refactor code to use a Class and
+#                               Functions for Assignment 06.
 
 
-wLoop = True
+class Tasks(object):
+    # Class for task list (to do list)
+    # Defines methods for managing list
 
-# Initial file scrub to create list/dictionary table
-objFile = open('ToDo.txt', 'r')
-lines = objFile.readlines()
-todoList = []
-for item in lines:
-    row = (item.split(','))
-    dictrow = {'Tasks': row[0].strip(), 'Priority': row[1].strip()}
-    todoList.append(dictrow)
-objFile.close()
+    # 0 - Read from file to create list/dictionary table
+    @staticmethod
+    def read_todo():
+        obj_file = open('ToDo.txt', 'r')  # renamed objFile to follow PEP 8
+        lines = obj_file.readlines()
+        todo_list = []  # renamed todoList to follow PEP 8
+        for item in lines:
+            row = (item.split(','))
+            dictrow = {'Tasks': row[0].strip(), 'Priority': row[1].strip()}
+            todo_list.append(dictrow)
+        obj_file.close()
+        return todo_list
 
-# Display To Do List to user
-print('\n---- Current To Do List: ----')
-for place in todoList:
-    print(place.get('Tasks') + ', ' + place.get('Priority'))
-print('-------- End of List --------\n')
+    # 1 - Show current list
+    @staticmethod
+    def view_todo(a):
+        todo_list = a
+        print('\n---- Current To Do List: ----')
+        for place in todo_list:
+            print(place.get('Tasks') + ', ' + place.get('Priority'))
+        print('-------- End of List --------\n')
 
-# Allow user to interact with To Do List or exit program
-while wLoop:
+    # 2 - Add new item to To Do List
+    @staticmethod
+    def add_task(b):
+        todo_list = b
+        task = input('Type task name: ')
+        priority = input('Type task priority: ')
+        dictrow = {'Tasks': task, 'Priority': priority}
+        todo_list.append(dictrow)
+        return todo_list
+
+    # 3 - Remove item from To Do List
+    @staticmethod
+    def rm_task(c):
+        todo_list = c
+        rmtask = input('Type the name of the task you want to remove: ')
+        for row in todo_list:
+            if row['Tasks'] == rmtask:
+                todo_list.remove(row)
+        return todo_list
+
+    # 4 - Save To Do List to file
+    @staticmethod
+    def save_todo(d):
+        todo_list = d
+        obj_file = open('ToDo.txt', 'w')
+        for place in todo_list:
+            obj_file.write(place.get('Tasks') + ', ' + place.get('Priority') + '\n')
+        obj_file.close()
+
+
+# Instantiate Tasks class as userList
+userList = Tasks()
+
+# Create initial to do list with the read_todo() method
+master_list = userList.read_todo()
+
+
+# Allow user to interact with to do list or exit program
+while True:
     print('---- To Do List Actions: ----')
     choice = int(input('1 - View current list\n'
                        '2 - Add new item\n'
@@ -37,38 +84,23 @@ while wLoop:
 
     # 1 - Show current list
     if choice == 1:
-        print('\n---- Current To Do List: ----')
-        for place in todoList:
-            print(place.get('Tasks') + ', ' + place.get('Priority'))
-        print('-------- End of List --------\n')
+        userList.view_todo(master_list)
 
     # 2 - Add new item to To Do List
     elif choice == 2:
-        task = input('Type task name: ')
-        priority = input('Type task priority: ')
-        dictrow = {'Tasks': task, 'Priority': priority}
-        todoList.append(dictrow)
+        master_list = userList.add_task(master_list)
 
     # 3 - Remove item from To Do List
     elif choice == 3:
-        rmtask = input('Type the name of the task you want to remove: ')
-        for row in todoList:
-            if row['Tasks'] == rmtask:
-                todoList.remove(row)
+        master_list = userList.rm_task(master_list)
 
     # 4 - Save To Do List to file
     elif choice == 4:
-        objFile = open('ToDo.txt', 'w')
-        for place in todoList:
-            objFile.write(place.get('Tasks') + ', ' + place.get('Priority') + '\n')
-        objFile.close()
+        userList.save_todo(master_list)
 
     # 5 - Save To Do List and Exit Program
     elif choice == 5:
-        objFile = open('ToDo.txt', 'w')
-        for place in todoList:
-            objFile.write(place.get('Tasks') + ', ' + place.get('Priority') + '\n')
-        objFile.close()
+        userList.save_todo(master_list)
         exit()
 
     # Handle bad user input
